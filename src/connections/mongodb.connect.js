5ca7bfe5859f6a314b90d109e0ee3245f9e07a41
pallab-nandi <pallabnandi@outlook.in>
2022-12-28 13:32:54 +0530

    production database added

--- src/connections/mongodb.connect.js
+++ src/connections/mongodb.connect.js
@@ -1,12 +1,14 @@
 const colors = require('colors');
 const mongoose = require('mongoose');
 
+const dbConfig = require('../configs/db.config');
+
 mongoose.set('strictQuery', true);
 
 // connecting to database
 const connect = () => {
     console.log('Initializing MongoDB connection...')
-    mongoose.connect('mongodb://127.0.0.1/air-india-clone')
+    mongoose.connect(dbConfig.production)
     .then(() => console.log('MongoDB connected successfully'.bold.cyan))
     .catch(err => console.log('Error while connecting to Database'.bold.red, err));
 }
