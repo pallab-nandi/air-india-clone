83fee16320b7e026d2340b3c570cbea3e48a7597
pallab-nandi <pallabnandi@outlook.in>
2023-03-17 14:35:25 +0530

    database modified

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    development : 'mongodb://127.0.0.1/air-india-clone',
-    production : 'mongodb+srv://pallab-nandi:root@air-india-clone.gbp2j1z.mongodb.net/air-india-app'
+    development: 'mongodb://127.0.0.1/air-india-clone',
+    production: 'mongodb+srv://pallab-nandi:root@projects.v7ehl3e.mongodb.net/air-india-app'
 }
